713d4c7cbb41c6a127bf6b5e37f674b57e694b21
Alya <114371692+alyaridwan27@users.noreply.github.com>
2024-06-19 21:57:17 +0700

    added origins

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["https://moringo-fe-eta.vercel.app"], 
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
